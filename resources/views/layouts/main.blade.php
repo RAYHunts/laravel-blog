43c8773791755d671b78fc22a53cf72ef49b4bc3
RAYHunts <151200078@student.upnyk.ac.id>
2022-05-20 01:32:38 +0700

    hello

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -54,7 +54,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/trix.css') }}">
     <script type="text/javascript" src="{{  asset('js/trix.js') }}"></script>
 </head>
-    <body class="bg-slate-400 dark:bg-gradient-to-br from-gray-800 via-slate-900 to-black font-monts scroll-y-beautify">
+    <body class="bg-slate-200 dark:bg-gradient-to-br from-gray-800 via-slate-900 to-black font-monts scroll-y-beautify">
         {{ $slot }}
     <script src="{{ asset('js/app.js') }}"></script>
     </body>
