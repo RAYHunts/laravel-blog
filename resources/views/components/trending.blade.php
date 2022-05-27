83f8aa5fea2a91d42e5c67238e8a278e34d01c64
RAYHunts <151200078@student.upnyk.ac.id>
2022-05-27 21:04:33 +0700

    hello

--- resources/views/components/trending.blade.php
+++ resources/views/components/trending.blade.php
@@ -38,15 +38,24 @@
     <div class=" py-4 font-bold text-3xl text-slate-700 dark:text-slate-400">
         <h1>Trendings <i class="fa-solid fa-arrow-trend-up"></i></h1>
     </div>
-    <ul>
-      @foreach ($trending as $article)
-      <li class="px-2 border-b">
-        <a href="{{ route('article', $article->slug) }}" class="text-lg text-slate-700 dark:text-slate-400 line-clamp-2">
-          {{ $article->title }}
-        </a>
-      </li>
-      @endforeach
-    </ul>
-  </div>
+        @foreach ($trending as $article)
+            <div class="px-8 py-4 bg-white rounded-lg shadow-md dark:bg-gray-800 ">
+                <div class="flex items-center justify-between">
+                    <span class="text-sm font-light text-gray-600 dark:text-gray-400">{{ $article->published_at->format('D, j M Y') }}</span>
+                    <a  href="{{ url('?category='.$article->category->slug) }}" class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500 uppercase">{{ $article->category->name }}</a>
+                </div>
+                <div class="mt-2">
+                    <a href="{{ route('article',$article->slug) }}" class="text-2xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline line-clamp-1">{{ $article->title }}</a>
+                </div>
+                <div class="flex items-center justify-between mt-4">
+                    <a href="{{ route('article',$article->slug) }}" class="text-blue-600 dark:text-blue-400 hover:underline">Read more</a>
+                    <div class="flex items-center">
+                    <img class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block" src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40&q=80" alt="avatar">
+                    <a class="font-bold text-gray-700 cursor-pointer dark:text-gray-200">{{ $article->author->name }}</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
     @endif
 @endif
